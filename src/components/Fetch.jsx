d1586552855f4debc5c9275d52fb235663aea365
natalia <nata774e@stud.kea.dk>
2022-11-24 17:53:26 +0100

    assets in public - links to svg work now

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -4,9 +4,7 @@ function Fetch() {
   return (
     <div className="fetchwrap">
       <h3>Your results....</h3>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 350" width="350" height="350">
-        <rect width="600" height="350" fill="#cccccc"></rect>
-      </svg>
+      <img src="/assets/03.svg" alt="Title image" />
       <h1>It will take X amount of trees</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, expedita ex deleniti optio dolorem autem!</p>
     </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,9 +2,7 @@ import Btn from "./Btn";
 function Intro() {
   return (
     <section className="intro">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 350" width="350" height="350">
-        <rect width="600" height="350" fill="#cccccc"></rect>
-      </svg>
+      <img src="/assets/title.svg" alt="Title image" />
       <Btn text="Calculate" />
     </section>
   );
