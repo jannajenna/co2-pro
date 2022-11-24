1a39ae9a3b31b05c4672038f33d9d8f7f17e9e64
anna <annabarellaalonso@gmail.com>
2022-11-24 13:39:06 +0100

    scss folder structure

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,7 +1,17 @@
 function Slide() {
   return (
     <div className="slide">
-      <input type="range" min="1" max="8" steps="1" />
+      <input type="range" min="0" max="8" steps="2" list="ticks" />
+      <datalist id="ticks">
+        <option value="0"></option>
+        <option value="1"></option>
+        <option value="2"></option>
+        <option value="3"></option>
+        <option value="4"></option>
+        <option value="5"></option>
+        <option value="6"></option>
+        <option value="7"></option>
+      </datalist>
     </div>
   );
 }
